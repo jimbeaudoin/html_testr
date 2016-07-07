dfa3bef4adb8688ff68218de75b656bc9ead5f87
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-07-07 17:41:41 -0400

    Add open and close js code for nav bar and add event to html to close sub menu

--- web/static/js/nav-bar.js
+++ web/static/js/nav-bar.js
@@ -1,12 +1,28 @@
 let NavBar = {
+  menuIsShown: false,
 
   init(element) {
     if(!element) { return; }
     let userMenu = document.getElementById("user-menu");
     let userSubMenu = document.getElementById("sub-menu");
+    let appHtml = document.getElementById("app-html");
 
     userMenu.addEventListener("click", e => {
-      userSubMenu.style.display = "block";
+      if (this.menuIsShown === true) {
+        userSubMenu.style.display = "none";
+      }
+      else {
+        userSubMenu.style.display = "block";
+      }
+      this.menuIsShown = !this.menuIsShown;
+      e.stopPropagation();
+    }),
+
+    appHtml.addEventListener("click", e => {
+      if (this.menuIsShown === true) {
+        userSubMenu.style.display = "none";
+        this.menuIsShown = false;
+      }
     })
   }
 }
